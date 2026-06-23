e53bfa5471b8019e5c1a8f3355bacd19a079a661
vinhnghi <tranluvinhnghi@gmail.com>
2026-06-23 17:56:31 +0700

    feat: add swagger bearer auth

--- cinema-be/src/main/java/com/cinema/config/OpenApiConfig.java
+++ cinema-be/src/main/java/com/cinema/config/OpenApiConfig.java
@@ -3,6 +3,8 @@ package com.cinema.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +19,14 @@ import org.springframework.context.annotation.Configuration;
         servers = {
                 @Server(url = "http://localhost:8080", description = "Local backend"),
                 @Server(url = "http://localhost:3000", description = "Local proxy or frontend")
-        }
+        },
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = io.swagger.v3.oas.annotations.enums.SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
 )
 public class OpenApiConfig {
 }
